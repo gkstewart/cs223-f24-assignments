38176969dd35ae2c600cff990c26f012cd9731fa
Gabrielle Katherine Marie Stewart <gkstewart@goldengate.cs.brynmawr.edu>
2024-09-20 19:25:12 -0400

    assignment 3 complete

--- A03/dynamic_songs.c
+++ A03/dynamic_songs.c
@@ -1,10 +1,103 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Gabrielle Stewart 
+ * Date: September 17th, 2024
+ * Description: This program reads songs from a text file into an array and then prints it out in a list.
  ---------------------------------------------*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// arrays to hold song information and variables
+typedef struct {
+    char title[128];   // Maximum length for the title
+    char artist[128];  // Maximum length for the artist
+    int duration_ms;
+    float danceability;
+    float energy;
+    float tempo;
+    float valence;
+} Song;
+
+// set up for later functions 
+int count_songs(FILE *file);
+void load_songs(FILE *file, Song *songs, int num_songs);
+void print_songs(Song *songs, int num_songs);
 
 int main() {
-  return 0;
+    FILE *file = fopen("songlist.csv", "r");
+    if (!file) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    // read songs from the first line of the file
+    int num_songs = count_songs(file);
+    if (num_songs <= 0) {
+        fprintf(stderr, "No songs found in file.\n");
+        fclose(file);
+        return 1;
+    }
+
+    // allocate memory for the songs
+    Song *songs = (Song *)malloc(num_songs * sizeof(Song));
+    if (!songs) {
+        perror("Memory allocation failed");
+        fclose(file);
+        return 1;
+    }
+
+    // load song data into the array
+    load_songs(file, songs, num_songs);
+
+    // print the songs
+    print_songs(songs, num_songs);
+
+    // free the memory and close the file
+    free(songs);
+    fclose(file);
+
+    return 0;
+}
+
+// function to count the number of songs in the file
+int count_songs(FILE *file) {
+    char buffer[256];
+    fgets(buffer, sizeof(buffer), file);  // read the first line
+    
+    int num_songs = atoi(strtok(buffer, ","));
+    
+    
+    fgets(buffer, sizeof(buffer), file);
+
+    return num_songs;
+}
+
+// function to load song data from the file
+void load_songs(FILE *file, Song *songs, int num_songs) {
+    char buffer[256];
+    for (int i = 0; i < num_songs; i++) {
+        fgets(buffer, sizeof(buffer), file);
+
+        // parse song data
+        strcpy(songs[i].title, strtok(buffer, ","));
+        strcpy(songs[i].artist, strtok(NULL, ","));
+        songs[i].duration_ms = atoi(strtok(NULL, ","));
+        songs[i].danceability = atof(strtok(NULL, ","));
+        songs[i].energy = atof(strtok(NULL, ","));
+        songs[i].tempo = atof(strtok(NULL, ","));
+        songs[i].valence = atof(strtok(NULL, ","));
+    }
+}
+
+// function to print the songs
+void print_songs(Song *songs, int num_songs) {
+    printf("Welcome to Dynamic Donna's Danceability Directory.\n\n");
+
+    for (int i = 0; i < num_songs; i++) {
+        int minutes = songs[i].duration_ms / 60000;
+        int seconds = (songs[i].duration_ms % 60000) / 1000;
+        printf("%d) %-30s artist: %-20s duration: %d:%02d D: %.3f E: %.3f T: %.3f V: %.3f\n", 
+               i, songs[i].title, songs[i].artist, minutes, seconds, 
+               songs[i].danceability, songs[i].energy, songs[i].tempo, songs[i].valence);
+    }
 }
--- A03/slow.c
+++ A03/slow.c
@@ -1,10 +1,64 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Gabrielle Stewart 
+ * Date: September 16th, 2024
+ * Description: This program turns a phrase into ent-speech.
  ---------------------------------------------*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
-  return 0;
+    // define the variables pauseLength and userText
+    int pauseLength;
+    char userText[32];
+
+    // ask the pause length from the user
+    printf("pause length? ");
+    scanf("%d", &pauseLength);
+
+    // ask for the text from the user
+    printf("text? ");
+    scanf("%s", userText);  
+
+    // find the length of the user text
+    int textLength = strlen(userText);
+
+    // calculating how long the new string will be
+    // each letter except the last one has 'pauseLength' . after
+    //  new size is textLength + (textLength - 1) * pauseLength + 1 (for '\0')
+    int newSize = textLength + (textLength - 1) * pauseLength + 1;
+
+    // allocate memory for  "ent-speech" string
+    char *entSpeech = (char *)malloc(newSize * sizeof(char));
+    if (entSpeech == NULL) {
+        printf("error: could not allocate memory\n");
+        return 1;  
+    }
+
+    //  the new string with pauses
+    int currentIndex = 0;
+    for (int i = 0; i < textLength; i++) {
+        // copy the letters
+        entSpeech[currentIndex] = userText[i];
+        currentIndex++;
+
+        // add dots after each letter
+        if (i < textLength - 1) {
+            for (int j = 0; j < pauseLength; j++) {
+                entSpeech[currentIndex] = '.';
+                currentIndex++;
+            }
+        }
+    }
+
+    // add the null character
+    entSpeech[currentIndex] = '\0';
+
+    // print line for  ent-speech string
+    printf("%s\n", entSpeech);
+
+    // free the memory 
+    free(entSpeech);
+
+    return 0; 
 }
